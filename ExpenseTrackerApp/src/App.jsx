116a3978323d41752280362fe2b643e5c19483e0
AbrarFaizMohammed <abrarfaizmohammed@gmail.com>
2023-08-08 14:11:32 -0400

    updates

--- ExpenseTrackerApp/src/App.jsx
+++ ExpenseTrackerApp/src/App.jsx
@@ -3,13 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import style from "./style.css";
 import Wrapper from './components/Wrapper';
-import { Routes,Route } from 'react-router-dom';
+// import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-   <Route  path='/' element={<Wrapper />} /> 
-    </Routes>
+    <Wrapper />
   )
 }
 
